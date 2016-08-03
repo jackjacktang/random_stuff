3e1068bc4e34bcb1b446170fdd2e5a3293bc1cc8
Jack Tang <jacktang@vlan-2642-10-16-175-128.student.wireless.sydney.edu.au>
2016-08-03 11:46:18 +1000

    puzzle working

--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,31 @@
 import numpy as np
 import scipy
 
-d = [0] * 10
-for a in range 10:
-	
-print(d)
+def check_digit(number,j):
+	temp = list(map(int, number))
+	count1 = temp.count(j)
+	count2 = temp[j]
+	print('digit: %d |count1: %d count2: %d' % (j,count1, count2))
+	if (count1 == count2):
+		return True
+	else:
+		return False
+
+
+def main():
+	flag = True
+	for i in range (6210000999,6210001001):
+		# print(i)
+		temp = str(i)
+		for j in range (0,10):
+			if check_digit(temp,j) != True:
+				flag = False
+				break
+			else:
+				flag = True
+		if (flag == True):
+			print('FOUND: %d' % i);
+
+if __name__ == "__main__":
+    main()
+
